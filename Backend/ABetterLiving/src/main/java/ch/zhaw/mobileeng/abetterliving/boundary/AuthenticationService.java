99c21f3921671e64d4886b6149bd420808992aaa
Raphael Fuchs <raphael.fuchs@outlook.com>
2018-07-03 23:47:05 +0200

    Backend finito

---
+++ Backend/ABetterLiving/src/main/java/ch/zhaw/mobileeng/abetterliving/boundary/AuthenticationService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.zhaw.mobileeng.abetterliving.boundary;
+
+/**
+ *
+ * @author Raphael Fuchs
+ */
+public class AuthenticationService {
+    
+}
--- Backend/ABetterLiving/src/main/java/ch/zhaw/mobileeng/abetterliving/boundary/UserService.java
+++ Backend/ABetterLiving/src/main/java/ch/zhaw/mobileeng/abetterliving/boundary/UserService.java
@@ -1,5 +1,7 @@
 package ch.zhaw.mobileeng.abetterliving.boundary;
 
+import ch.zhaw.mobileeng.abetterliving.model.Users;
+import ch.zhaw.mobileeng.abetterliving.repository.UserRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,20 +14,71 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Path("/user")
 public class UserService {
-    /*@POST
-    @Path("/login")
-    @Consumes(MediaType.APPLICATION_JSON)
+
+    @Autowired
+    private UserRepository listRepository;
+
+    @GET
+    @Path("/all")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<User> checkUserLogin() {
-        List<Part> resList = new ArrayList<>();
-        for (String s : InventoryRepository.getInstance().getParts().keySet()) {
-            resList.add(InventoryRepository.getInstance().getParts().get(s));
+    public List<Users> getAllEntries() {
+        List<Users> resList = new ArrayList<>();
+        for (Users u : listRepository.findAll()) {
+            resList.add(u);
         }
         return resList;
-    }*/
+    }
+
+    @GET
+    @Path("/id/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Object getEntryByID(@PathParam("id") Long id) {
+        try {
+            return listRepository.findById(id).get();
+        } catch (Exception e) {
+            return ResponseHandler.response("Request failed", e.getMessage(), "get", false, null);
+        }
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Object updateEntry(Users u, @PathParam("id") Long id) {
+        try {
+            u.setUserID(id);
+            Users saved = listRepository.save(u);
+            return ResponseHandler.response("Update successful", "", "put", true, saved);
+        } catch (Exception e) {
+            return ResponseHandler.response("Update failed", e.getMessage(), "put", false, null);
+        }
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Object addEntry(Users u) {
+        try {
+            u.setUserID(null);
+            Users saved = listRepository.save(u);
+            return ResponseHandler.response("Insert successful", "", "post", true, saved);
+        } catch (Exception e) {
+            return ResponseHandler.response("Insert failed", e.getMessage(), "post", false, null);
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Object deleteEntry(@PathParam("id") Long id) {
+        try {
+            listRepository.deleteById(id);
+            return ResponseHandler.response("Delete successful", "", "post", true, null);
+        } catch (Exception e) {
+            return ResponseHandler.response("Delete failed", e.getMessage(), "post", false, null);
+        }
+    }
 }
